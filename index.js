285396c6acdf10d5fe22c6a8c577413d2df32af4
Travis Peterson <travis.peterson@ave81.com>
2015-10-23 13:44:13 -0500

    fixes watch to work with globs

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var watchify = require('watchify');
 var babelify = require('babelify');
 var uglify = require('gulp-uglify');
 var _ = require('lodash');
-var through = require('through2');
 var globby = require('globby');
 
 var source = require('vinyl-source-stream');
@@ -22,48 +21,43 @@ module.exports = function(buildConfig, watch){
 
   if (_.isString(bundleEntries)) bundleEntries = [bundleEntries];
 
-  var bundleStream = through();
+  var entries = globby.sync(bundleEntries);
+  var bundleOptions = _.extend({
+    entries: entries,
+    insertGlobals: true,
+    debug: DEBUG
+  }, watchify.args, (buildConfig.bundleOptions || {}));
+  // set up the browserify instance on a task basis
+  var b = browserify(bundleOptions);
 
-  bundleStream
-    .pipe(source(buildConfig.src))
-    .pipe(buffer())
-    .pipe(rename(buildConfig.bundleName))
-    .pipe(gulp.dest(buildDir))
-    .pipe(gulpif(!DEBUG, uglify()))
-    .pipe(gulp.dest(dest));
+  // apply the transforms here to fix problem with babelify recursively
+  //  calling itself and multiplying build times
+  b.transform(babelify.configure({
+    ignore: /(node_modules)|(vendor)|(static)|(build)/,
+    sourceMaps: 'inline'
+  }));
 
-  globby(['./tests/*.js']).then(function(entries){
-    var bundleOptions = _.extend({
-      entries: entries,
-      insertGlobals: true,
-      debug: DEBUG
-    }, watchify.args, (buildConfig.bundleOptions || {}));
-    // set up the browserify instance on a task basis
-    var b = browserify(bundleOptions);
+  // wrap browserify in watchify for faster builds
+  if (watch) {
+    b = watchify(b);
+    b.on('update', bundle);
+    b.on('log', function(msg){
+      gutil.log('Change detected in source for bundle: '+buildConfig.bundleName);
+      gutil.log('Updating Bundle...');
+      gutil.log(msg);
+    });
+  }
 
-    // apply the transforms here to fix problem with babelify recursively
-    //  calling itself and multiplying build times
-    b.transform(babelify.configure({
-      ignore: /(node_modules)|(vendor)|(static)|(build)/,
-      sourceMaps: 'inline'
-    }));
+  function bundle(){
+    return b.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source(buildConfig.src))
+      .pipe(buffer())
+      .pipe(rename(buildConfig.bundleName))
+      .pipe(gulp.dest(buildDir))
+      .pipe(gulpif(!DEBUG, uglify()))
+      .pipe(gulp.dest(dest));
+  }
 
-    // wrap browserify in watchify for faster builds
-    if (watch) {
-      b = watchify(b);
-      b.on('update', bundle);
-      b.on('log', function(msg){
-        gutil.log('Change detected in source for bundle: '+buildConfig.bundleName);
-        gutil.log('Updating Bundle...');
-        gutil.log(msg);
-      });
-    }
-
-
-    b.bundle().pipe(bundleStream);
-  }).catch(function(err){
-    bundleStream.emit('error', err);
-  });
-
-  return bundleStream;
+  return bundle();
 }
